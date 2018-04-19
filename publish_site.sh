199b7daf3848777b1b30feea3457e86dbd6448c5
Phillip J. Wolfram <phillipwolfram@gmail.com>
2018-04-19 11:43:42 -0600

    Script to publish the site

---
+++ publish_site.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+#https://www.sitepoint.com/jekyll-plugins-github/
+# build site
+rake build
+
+cd ../compiled_site
+git add --all
+git commit -m "Published `date`"
+git push origin master:master
+
