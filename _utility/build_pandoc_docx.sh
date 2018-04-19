8792a14906c9ff3f0e69fd657f30390deeb488f2
Phillip J. Wolfram <phillipwolfram@gmail.com>
2018-04-19 11:40:07 -0600

    Cleans repo

--- _utility/build_pandoc_docx.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# This builds a Word doc from Pandoc markdown (argument to script for EXAMPLE.md file)
-
-OUTEXTENSION=docx
-INFILE=$1
-OUTFILE=${INFILE%.*}.$OUTEXTENSION
-
-pandoc -o $OUTFILE -f markdown -t $OUTEXTENSION $INFILE --filter=pandoc-fignos --bibliography=../_bibliography/references.bib  --csl=../_bibliography/nature
--- light.md
+++
@@ -1,1 +0,0 @@
-_research/LIGHT.md
