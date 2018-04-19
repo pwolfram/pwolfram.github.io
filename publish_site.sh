b60bd968c8d4d661005f431e53052432e694c950
Phillip J. Wolfram <phillipwolfram@gmail.com>
2018-04-19 14:07:32 -0600

    Published version v2.0.0-45-gbc76d66 on Thu Apr 19 14:07:27 MDT 2018

--- publish_site.sh
+++ publish_site.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
+DATE=`date`
+VERSION=`git describe --tags --dirty`
+echo "Don't forget to publish source changes!"
+
 #https://www.sitepoint.com/jekyll-plugins-github/
 # build site
 rake build
 
 cd ../compiled_site
 git add --all
-git commit -m "Published `date`"
+git commit -m "Published version ${VERSION} on ${DATE}"
 git push origin master:master
 
